9831f7389f548733aa9df182b68ff01f3c58a197
blueberryade <115989049+blueberryade@users.noreply.github.com>
2023-05-18 19:19:29 +0900

    [unrated] Title: A 강조하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,11 @@
+function solution(myString) {
+    var answer = '';
+    for(let i of myString){
+       if(i==="a" || i==="A"){
+           answer += "A"
+       } else {
+           answer += i.toLowerCase()
+       }
+    }
+    return answer;
+}
