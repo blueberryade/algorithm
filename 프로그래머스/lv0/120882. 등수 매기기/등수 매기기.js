19c0d281557fd3abc0b4843c321fb3f37549372c
blueberryade <115989049+blueberryade@users.noreply.github.com>
2023-05-21 14:55:31 +0900

    [level 0] Title: 등수 매기기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,11 @@
+function solution(score) {
+    var answer = Array(score.length).fill(1)
+    for(let i=0;i<score.length;i++){
+        for(let j=0;j<score.length;j++){
+            if(score[i][0]+score[i][1] > score[j][0]+score[j][1]){
+                answer[j]++
+            }
+        }
+    }
+    return answer;
+}
