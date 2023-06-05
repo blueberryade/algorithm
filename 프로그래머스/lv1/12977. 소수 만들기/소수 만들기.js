3a49447ffdc43edb3555bbdeb2b0b91c669d2918
blueberryade <115989049+blueberryade@users.noreply.github.com>
2023-06-05 23:14:13 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,18 @@
+function solution(nums) {
+    var answer = 0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            for(let k=j+1;k<nums.length;k++){
+                const sum = nums[i]+nums[j]+nums[k];
+                if(isPrime(sum)) answer+=1
+            }
+        }
+    }
+    return answer;
+}
+function isPrime(sum){
+    for(let i=2;i<sum;i++){
+        if(sum%i===0) return false;
+    }
+    return true;
+}
