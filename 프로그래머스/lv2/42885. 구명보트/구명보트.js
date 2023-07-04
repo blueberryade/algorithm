c33a68c27efbb2f19a1b4991afa4455e8ca1abe8
blueberryade <115989049+blueberryade@users.noreply.github.com>
2023-07-04 21:13:17 +0900

    [level 2] Title: 구명보트, Time: 10.96 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -0,0 +1,15 @@
+function solution(people, limit) {
+    var answer = 0;
+    people.sort((a,b)=>a-b)
+    while(people.length){
+        if(people[0]+people[people.length-1] <= limit){
+            people.pop();
+            people.shift();
+            answer++
+        } else{
+            people.pop();
+            answer++
+        }
+    }
+    return answer;
+}
