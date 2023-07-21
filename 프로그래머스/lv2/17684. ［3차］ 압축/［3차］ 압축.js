d181b76b05c208cd0bea25d177fc0c868211ea1f
blueberryade <115989049+blueberryade@users.noreply.github.com>
2023-07-21 20:21:02 +0900

    [level 2] Title: [3차] 압축, Time: 0.47 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,16 @@
+function solution(msg) {
+    var answer = [];
+    let words = ['A','B','C','D','E','F','G','H','I','J','K',"L",'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let before = '';
+    for(let i=0;i< msg.length;i++){
+        before+=msg[i]
+        if(!words.includes(before)){
+            answer.push(words.indexOf(before.slice(0,-1))+1)
+            words.push(before)
+            before = msg[i]
+        }
+    }
+    
+    if(before) answer.push(words.indexOf(before)+1)
+    return answer;
+}
