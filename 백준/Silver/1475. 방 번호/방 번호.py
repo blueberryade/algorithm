e14f4172a207224feafec3a8b20569a1bc6e892a
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-01-29 17:49:29 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,11 @@
+num = input()
+lst = [0]*10
+for i in num:
+    if i == '6' or i == '9':
+        if lst[6] <= lst[9]:
+            lst[6]+=1
+        else:
+            lst[9]+=1
+    else:
+        lst[int(i)] +=1
+print(max(lst))
