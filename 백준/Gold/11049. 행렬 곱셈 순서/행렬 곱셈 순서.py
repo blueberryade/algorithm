dee1e2a2dd265a74ef8ff6b8bd8709ab2335efcb
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-04-03 14:49:04 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 3148 ms, Memory: 36664 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,26 +1,14 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-M = []
-D = [[-1 for j in range(N+1)] for i in range(N+1)]
+n = int(input())
 
-M.append((0,0))
+arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [a for a, _ in arr] + [arr[-1][1]]
+dp = [[0] * n for _ in range(n)]
 
-for i in range(N):
-    x,y = map(int,input().split())
-    M.append((x,y))
+for step in range(1,n):
+    for loc in range(n-step):
+        end = loc + step
+        mul = arr[loc] * arr[end+1]
+        minimum =  min(yk + xk + sz * mul for yk, xk, sz in zip(dp[loc][loc:end], dp[end][loc+1:end+1], arr[loc+1:end+1]))
+        dp[loc][end] = dp[end][loc] = minimum
 
-def execute(s,e):
-    result = sys.maxsize
-    if D[s][e]!=-1:
-        return D[s][e]
-    if s == e:
-        return 0
-    if s+1 == e:
-        return M[s][0]*M[s][1]*M[e][1]
-    for i in range(s,e):
-        result = min(result,M[s][0]*M[i][1]*M[e][1]+execute(s,i)+execute(i+1,e))
-    D[s][e] = result
-    return D[s][e]
-
-print(execute(1,N))
+print(dp[0][-1])
