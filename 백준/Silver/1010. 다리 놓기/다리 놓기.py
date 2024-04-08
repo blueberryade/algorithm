086a82cae5462a0d8b4c26c4d50b5e037669c9c9
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-04-08 09:59:36 +0900

    [Silver V] Title: 다리 놓기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+D = [[0 for _ in range(31)] for _ in range(31)]
+
+for i in range(0,31):
+    D[i][1] = i
+    D[i][0] = 1
+    D[i][i] = 1
+
+for i in range(2,31):
+    for j in range(1,i):
+        D[i][j]= D[i-1][j]+D[i-1][j-1]
+
+T = int(input())
+
+for i in range(T):
+    N,M = map(int,input().split())
+    print(D[M][N])
+
