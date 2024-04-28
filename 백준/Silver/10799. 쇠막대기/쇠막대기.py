7bab25ac02ff4f52f3cc879b7e048f9124ae3ef0
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-04-28 22:00:57 +0900

    [Silver II] Title: 쇠막대기, Time: 60 ms, Memory: 31552 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,18 @@
+lst = input()
+
+stack = []
+result = 0
+
+for i in range(len(lst)):
+    if lst[i] == '(':
+        stack.append('(')
+    else:
+        if lst[i-1] == '(':
+            stack.pop()
+            result+=len(stack)
+        else:
+            stack.pop()
+            result+=1
+
+    
+print(result)
