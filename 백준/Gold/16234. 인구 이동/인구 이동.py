bddec1e119d6b9b324ee87d88477a290211c6806
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-05-05 20:51:21 +0900

    [Gold IV] Title: 인구 이동, Time: 5716 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -0,0 +1,54 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N,L,R = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(N)]
+
+
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
+
+result = 0
+
+
+def bfs(x,y,index):    
+    united = []
+    united.append((x,y))
+    q = deque()
+    q.append((x,y))
+    union[x][y] = index
+    summary = graph[x][y]
+    count = 1
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<N and 0<=ny<N and union[nx][ny]==-1:
+                if L<=abs(graph[nx][ny]-graph[x][y])<=R:
+                    q.append((nx,ny))
+                    union[nx][ny] = index
+                    summary+= graph[nx][ny]
+                    count+=1
+                    united.append((nx,ny))
+    for i,j in united:
+        graph[i][j] = summary // count
+    return count
+                
+    
+total_count = 0    
+
+while True:
+    union = [[-1] *N for _ in range(N)]
+    index = 0
+    for i in range(N):
+        for j in range(N):
+            if union[i][j] == -1:
+                bfs(i,j,index)
+                index+=1
+    if index == N*N:
+        break
+    total_count +=1
+
+print(total_count)
