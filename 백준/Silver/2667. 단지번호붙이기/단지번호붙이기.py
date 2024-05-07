c94bae3776e6cc8c3a872f3c1afdd313b05176b3
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-05-07 22:36:18 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,37 @@
-from collections import deque
+import sys
+n = int(sys.stdin.readline())
 
-n = int(input())
-
-graph = [list(map(int,input())) for _ in range(n)]
-
-cnt = []
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
+result = []
+cnt = 0
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
-def bfs(x,y):
-    q = deque()
-    q.append((x,y))
-    graph[x][y] = 0
-    cnt = 1
-    
-    while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+def dfs(x, y):
+    global cnt
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return
 
-            if 0<=nx<n and 0<=ny<n and graph[nx][ny]:
-                graph[nx][ny] = 0
-                q.append((nx,ny))
-                cnt +=1
-    return cnt      
+    if graph[x][y] == 1:
+        cnt += 1
+        graph[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            dfs(nx, ny)
+
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1:
-            cnt.append(bfs(i,j))
+        if graph[i][j]:
+            dfs(i,j)
+            result.append(cnt)
+            cnt = 0
 
+result.sort()
 
-cnt.sort()
-print(len(cnt))
-for c in cnt:
-    print(c)
+print(len(result))
 
+for i in result:
+    print(i)
