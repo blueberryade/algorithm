464961674ee929eceed29ab74dd961c96350ed12
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-05-21 21:46:01 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+board = [0]*101
+visited = [False]* 101
+
+ladder = dict()
+snake = dict()
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    ladder[a] = b
+for _ in range(m):
+    a,b = map(int,input().split())
+    snake[a] = b
+
+q = deque([1])
+
+while q:
+    x = q.popleft()
+    if x == 100:
+        print(board[x])
+    for d in range(1,7):
+        next_place = x + d
+        if next_place <= 100 and not visited[next_place]:
+            if next_place in ladder.keys():
+                next_place = ladder[next_place]
+            if next_place in snake.keys():
+                next_place = snake[next_place]
+            if not visited[next_place]:
+                visited[next_place] = True
+                board[next_place] = board[x]+1
+                q.append(next_place)
