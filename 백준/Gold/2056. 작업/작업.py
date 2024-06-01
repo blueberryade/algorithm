a2536b2eddb20da609a49e5ba65efdbdb9f4933d
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-06-01 13:19:17 +0900

    [Gold IV] Title: 작업, Time: 440 ms, Memory: 54272 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.py
+++ 백준/Gold/2056. 작업/작업.py
@@ -1,32 +1,24 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-indegree = [0]*(n+1)
-graph = [[] for _ in range(n+1)]
-time = [0]*(n+1)
+graph = {}
+times = [0]*(n+1)
 
 for i in range(1,n+1):
     data = list(map(int,input().split()))
-    time[i] = data[0]
-    for j in range(1,data[1]+1):
-        graph[data[j+1]].append(i)
-        indegree[i]+=1
-
-q = deque()
-result = [0]*(n+1)
-
+    times[i] = data[0]
+    if data[1] !=0:
+       for j in data[2:]:
+           if i in graph:
+                graph[i].append(j)
+           else:
+               graph[i] = [j]
+				
 for i in range(1,n+1):
-    if indegree[i] == 0:
-        q.append(i)
-        result[i] = time[i]
-
-while q:
-    now = q.popleft()
-    for i in graph[now]:
-        result[i] = max(result[i],result[now]+time[i])
-        indegree[i]-=1
-        if indegree[i] == 0:
-            q.append(i) 
-print(max(result))           
+	if i in graph:
+		time = 0
+		for j in graph[i]:
+			time = max(time,times[j])
+		times[i]+=time
+print(max(times))						  
