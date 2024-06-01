001c381f9bebe7fdc087c5fb49f527489689baac
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-06-01 12:29:07 +0900

    [Gold III] Title: 음악프로그램, Time: 64 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+n,m = map(int, input().split())
+indegree = [0]*(n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    data = list(map(int,input().split()))
+    for i in range(1,data[0]):
+        graph[data[i]].append(data[i+1])
+        indegree[data[i+1]]+=1  
+
+q = deque()
+result = []
+
+for i in range(1,n+1):
+    if indegree[i] == 0:
+        q.append(i)
+        
+while q:
+    now = q.popleft()
+    result.append(now)
+    for i in graph[now]:
+        indegree[i]-=1
+        if indegree[i] == 0:
+            q.append(i)
+
+if len(result)!=n:
+    print(0)
+else:
+    for i in result:
+        print(i)
