973af1d6db00d90affa21e99df55b4da5a8b95ad
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-06-04 15:08:28 +0900

    [Silver II] Title: 섬의 개수, Time: 60 ms, Memory: 31500 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,25 +1,17 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
-
-def bfs(i,j):
+def dfs(x,y):
     dx = [1, -1, 0, 0, 1, -1, 1, -1]
     dy = [0, 0, -1, 1, -1, 1, 1, -1]
-    graph[i][j] = 0
-    q = deque()
-    q.append((i,j))
-    
-    while q:
-        x,y = q.popleft()
-        for i in range(8):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if 0<=nx<h and 0<=ny<w and graph[nx][ny] == 1 :
-                    q.append((nx,ny))
-                    graph[nx][ny] = 0
 
-
+    graph[x][y] = 0
+    for i in range(8):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if 0 <=nx<h and 0<=ny<w and graph[nx][ny] == 1 :
+            dfs(nx,ny)
 
 while True:
     cnt = 0
@@ -31,8 +23,7 @@ while True:
 
     for i in range(h):
         for j in range(w):
-            if graph[i][j] == 1 :
-                bfs(i,j)
+            if graph[i][j] == 1:
+                dfs(i,j)
                 cnt+=1
     print(cnt)         
-
