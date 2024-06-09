bf950a272b558c2bf633c0649862883a7569aa93
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-06-09 10:36:21 +0900

    [Gold II] Title: 피보나치 수 6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,26 @@
+n = int(input())
+p = 1000000007
+
+dp = dict()
+
+def fibo(n):
+    if dp.get(n) != None:
+        return dp[n]
+    
+    if n == 0:
+        return 0
+    
+    if n == 1 or n == 2:
+        return 1
+    
+    if n % 2 ==0:
+        dp[n // 2 + 1] = fibo(n // 2 + 1) % p
+        dp[n // 2 - 1] = fibo(n // 2 - 1) % p
+        return dp[n // 2 + 1] ** 2 - dp[n // 2 - 1] ** 2
+    
+    else:
+        dp[n // 2 + 1] = fibo(n // 2 + 1) % p
+        dp[n // 2] = fibo(n // 2) % p
+        return dp[n // 2 + 1] ** 2 + dp[n // 2] ** 2
+
+print(fibo(n)%p)
