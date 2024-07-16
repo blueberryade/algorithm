47b02ae7740a35182800583aa47a1b82b0798b68
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-07-16 17:21:37 +0900

    [Gold IV] Title: 네트워크 연결, Time: 272 ms, Memory: 46628 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n = int(input())
+m = int(input())
+
+parent = [i for i in range(n+1)]
+edges = []
+for _ in range(m):
+    a,b,cost = map(int,input().split())
+    edges.append((cost,a,b))
+
+edges.sort()
+
+result = 0
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
