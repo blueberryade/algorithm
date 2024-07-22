1fb57976b612fb1449e0bdcd022076a0bb6a024b
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-07-22 11:35:26 +0900

    [Gold III] Title: 크게 만들기, Time: 180 ms, Memory: 34544 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+numbers = input().rstrip()
+stack = []
+
+for num in numbers:
+    while stack and stack[-1] < num and k > 0:
+        stack.pop()
+        k-=1
+    stack.append(num)
+
+if k > 0:
+    print(''.join(stack[:-k]))
+else:
+    print(''.join(stack))
