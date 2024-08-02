b328a5741d96142627418dda888bd1ab18d879c8
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-08-02 14:09:23 +0900

    [Gold IV] Title: 주난의 난(難), Time: 172 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.py
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.py
@@ -1,5 +1,5 @@
 import sys
-import heapq
+from collections import deque
 input = sys.stdin.readline
 INF = sys.maxsize
 
@@ -9,28 +9,29 @@ graph = [list(input().rstrip()) for _ in range(n)]
 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-distance = [[INF]*m for _ in range(n)]
 
-
-def dijkstra(x1,y1):
-    q = []
-    heapq.heappush(q,(0,x1,y1))
+def bfs(x1,y1):
+    distance = [[INF]*m for _ in range(n)]
+    q = deque()
+    q.append((x1,y1))
     distance[x1][y1] = 0
 
     while q:
-        cnt,x,y = heapq.heappop(q)
-
-        if distance[x][y] < cnt:
-            continue
+        x,y = q.popleft()
 
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
             if 0<=nx<n and 0<=ny<m:
-                cost = cnt+(graph[nx][ny]!='0')
-                if cost<distance[nx][ny]:
-                    heapq.heappush(q,(cost,nx,ny))
-                    distance[nx][ny] = cost
+                cost = 1 if graph[nx][ny]!='0' else 0
+                if cost + distance[x][y] < distance[nx][ny]:
+                    distance[nx][ny] = cost + distance[x][y]
+                    if cost == 1:
+                        q.append((nx,ny))
+                    else:
+                        q.appendleft((nx,ny))
 
-dijkstra(x1-1,y1-1)
-print(distance[x2-1][y2-1])
+    return distance[x2-1][y2-1]
+    
+                   
+print(bfs(x1-1,y1-1))
