c095b02d2bfce5d214b4b7fa8b02e0dafe367f13
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-08-21 13:31:54 +0900

    [Gold IV] Title: 회사 문화 1, Time: 268 ms, Memory: 70340 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -1,12 +1,25 @@
-n,m = map(int,input().split())
-superior = [0]+list(map(int,input().split()))
-result = [0]*(n+1)
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
+n, m = map(int, input().split())
+superior = list(map(int, input().split()))
+
+graph = [[] for _ in range(n+1)]
+for i in range(2, n+1):  
+    graph[superior[i-1]].append(i)  
+
+result = [0] * (n + 1)
 for _ in range(m):
-    i,w = map(int,input().split())
-    result[i]+=w
+    i, w = map(int, input().split())
+    result[i] += w
 
-for i in range(2,n+1):
-    result[i]+=result[superior[i]]
 
-print(*result[1:])
+def dfs(node):
+    for child in graph[node]:
+        result[child] += result[node] 
+        dfs(child)
+
+dfs(1)
+
+print(*result[1:])  
