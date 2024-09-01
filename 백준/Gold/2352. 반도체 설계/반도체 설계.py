b802cc776a2677f44525dd6d080fbab6583b84b7
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-09-01 10:15:10 +0900

    [Gold II] Title: 반도체 설계, Time: 60 ms, Memory: 37968 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data = list(map(int,input().split()))
+
+lst = []
+
+for i in data:
+    idx = bisect_left(lst,i)
+    if idx == len(lst):
+        lst.append(i)
+    else:
+        lst[idx] = i
+print(len(lst))    
