c82d5291c0849ea997d26041c2a9bca0774f6631
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-09-02 15:44:07 +0900

    [Gold I] Title: 택배, Time: 32 ms, Memory: 31120 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Gold/8980. 택배/택배.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n,c = map(int,input().split())
+m = int(input())
+lst = []
+for _ in range(m):
+    s,e,box = map(int,input().split())
+    lst.append((s,e,box))
+
+lst.sort()
+
+capacity = [c]*n
+result = 0
+
+for s,e,box in lst:
+    temp = c
+    for i in range(s,e):
+        if temp>min(capacity[i],box):
+            temp = min(capacity[i],box)
+    for i in range(s,e):
+        capacity[i]-=temp
+    result+=temp
+            
+print(result)
