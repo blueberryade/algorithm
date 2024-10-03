6a50ceaf579cdaeb1eb3824ff50a5b7148cf2f05
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-10-03 11:42:11 +0900

    [Gold V] Title: 감소하는 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+
+n = int(input())
+answer = []
+
+for i in range(1,11):
+    for j in combinations(range(10),i):
+        num = sorted(list(j), reverse=True)
+        answer.append(int("".join(map(str,num))))
+
+answer.sort()
+print(answer[n] if len(answer) > n else -1)
