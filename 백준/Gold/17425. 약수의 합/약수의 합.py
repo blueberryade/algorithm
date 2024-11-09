918255f111a75a7382da4a591028835c776227cf
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-11-09 21:56:16 +0900

    [Gold IV] Title: 약수의 합, Time: 224 ms, Memory: 126176 KB -BaekjoonHub

---
+++ 백준/Gold/17425. 약수의 합/약수의 합.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+MAX = 1000000
+
+dp = [0]*(MAX+1)
+s = [0]*(MAX+1)
+
+for i in range(1,MAX+1):
+    j = 1
+    while i*j <= MAX:
+        dp[i*j]+=i
+        j+=1
+
+for i in range(1,MAX+1):
+    s[i] = s[i-1] + dp[i]
+
+
+t = int(input())
+
+for _ in range(t):
+    N = int(input())
+    print(s[N])
+
+
