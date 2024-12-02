604673ab43e4a70f458afd7867404e52f7eaee09
blueberryade <115989049+blueberryade@users.noreply.github.com>
2024-12-02 11:45:54 +0900

    [Gold V] Title: AC, Time: 172 ms, Memory: 43340 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -2,34 +2,34 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-t = int(input())
-
-for _ in range(t):
-    p = input().rstrip()
+T = int(input())
+for _ in range(T):
+    p = list(input().rstrip())
     n = int(input())
-    arr = deque(input().rstrip()[1:-1].split(','))
-    if n == 0:
-        arr = deque()
-
+    q = deque(input().rstrip()[1:-1].split(","))
+    flag = True
     r = 0
-    flag = 0
 
-    for f in p:
-        if f == 'D':
-            if len(arr) == 0:
-                flag = 1
+    if n == 0:
+        q = deque()
+    
+    for i in p:
+        if i == 'D':
+            if len(q) == 0:
+                flag = False
                 print('error')
                 break
             else:
-                if r%2 == 0:
-                    arr.popleft()
+                if r%2==0:
+                    q.popleft()
                 else:
-                    arr.pop()
+                    q.pop()
         else:
-            r+=1                   
-    if flag == 0:
-        if r % 2 == 0:
-            print('['+','.join(arr)+']')     
+            r+=1
+
+    if flag:
+        if r%2 == 0:
+            print('['+','.join(q)+']') 
         else:
-            arr.reverse()
-            print('['+','.join(arr)+']')
+            q.reverse()
+            print('['+','.join(q)+']') 
