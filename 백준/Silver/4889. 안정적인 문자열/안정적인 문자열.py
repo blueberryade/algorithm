a1b3276882b9727b8fc180144bb65a2a4c444b95
blueberryade <115989049+blueberryade@users.noreply.github.com>
2025-01-18 23:34:04 +0900

    [Silver I] Title: 안정적인 문자열, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.py
@@ -0,0 +1,21 @@
+result = []
+
+while True:
+    stack = []
+    count = 0
+    s = input()
+    if s[0] == '-':
+        break
+    for i in range(len(s)):
+        if not stack and s[i]=='}':
+            count+=1
+            stack.append('{')
+        elif stack and s[i] == '}':
+            stack.pop()
+        else:
+            stack.append(s[i])
+    count+=len(stack)//2
+    result.append(count)
+
+for i in range(len(result)):
+    print(i+1,'. ',result[i],sep='')
