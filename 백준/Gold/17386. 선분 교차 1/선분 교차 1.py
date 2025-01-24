7e648f1ed2294073cd52d43729e7c89582044668
blueberryade <115989049+blueberryade@users.noreply.github.com>
2025-01-24 23:23:00 +0900

    [Gold III] Title: 선분 교차 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+x1, y1, x2, y2 = map(int,input().split())
+x3, y3, x4, y4 = map(int,input().split())
+
+
+def CCW(x1,y1,x2,y2,x3,y3):
+    return (x1*y2 + x2*y3 + x3*y1) - (x2*y1+x3*y2+x1*y3)
+
+a = CCW(x1, y1, x2, y2, x3, y3)
+b = CCW(x1, y1, x2, y2, x4, y4)
+c = CCW(x3, y3, x4, y4, x1, y1)
+d = CCW(x3, y3, x4, y4, x2, y2)
+
+if a*b < 0 and c*d < 0:
+    print(1)
+else:
+    print(0)
